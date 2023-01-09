7002ad23421f1eb52c44381e27c61f86f18f4efa
dozero25 <ehdud05264@naver.com>
2023-01-09 21:37:11 +0900

    1/9 user_mst 저장

--- user_mst.sql
+++ user_mst.sql
@@ -1,1 +1,18 @@
 SELECT * FROM library.user_mst;
+SELECT * FROM library.role_dtl;
+
+insert into user_mst
+values(0, "aaa", "1234", "aaa", "aaa@gmail.com", now(), now());
+
+insert into role_dtl
+values(0, 1, 2, now(), now());
+
+
+select
+	*
+from
+	user_mst um
+    left outer join role_dtl rd on(rd.user_id = um.user_id)
+    left outer join role_mst rm on(rm.role_id = rd.role_id)
+where
+	um.username = 'aaa';

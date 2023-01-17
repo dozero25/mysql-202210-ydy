b231e86d186863fb87622659dc8659a6ca8ac204
dozero25 <ehdud05264@naver.com>
2023-01-17 10:24:21 +0900

    1/17 book_mst

--- home/book_mst.sql
+++ home/book_mst.sql
@@ -1,2 +1,10 @@
 SELECT * FROM library.book_mst;
 
+select * from book_mst
+where publication_date like '2110%';
+
+update book_mst set publication_date = ('2010-05-23')
+where publication_date like '2110%';
+
+select * from book_mst
+order by publication_date Desc;

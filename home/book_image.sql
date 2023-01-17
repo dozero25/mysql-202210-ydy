f9a09edd8991a71980cd2edbb703f80a3bff6a7a
dozero25 <ehdud05264@naver.com>
2023-01-17 12:00:40 +0900

    book_image

--- home/book_image.sql
+++ home/book_image.sql
@@ -1,1 +1,16 @@
 SELECT * FROM library.book_image;
+
+insert into book_image
+value
+	(0, '소록-999', '가짜이름1.png', '진짜이름1.png'),
+	(0, '소록-999', '가짜이름2.png', '진짜이름2.png'),
+	(0, '소록-999', '가짜이름3.png', '진짜이름3.png'),
+	(0, '소록-999', '가짜이름4.png', '진짜이름4.png');
+    
+select
+	*
+from
+	book_mst bm
+    left outer join book_image bi on(bi.book_code = bm.book_code)
+where
+	bm.book_code = '소록-999';

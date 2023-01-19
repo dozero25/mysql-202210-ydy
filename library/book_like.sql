5498b8ace6b88aa6d8f3888889643ab66b446c09
dozero25 <ehdud05264@naver.com>
2023-01-19 21:17:14 +0900

    book_like

---
+++ library/book_like.sql
@@ -0,0 +1,11 @@
+SELECT * FROM library.book_like;
+
+select
+	*
+from
+	book_mst bm
+    left outer join book_like bl on(bl.book_id = bm.book_id and bl.user_id = 1);
+    
+insert into book_like
+values
+	(0, 4, 1);

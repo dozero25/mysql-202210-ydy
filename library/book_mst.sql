e96f6fe1811251c1a9e2e1b435e78fcdd1c50c3a
dozero25 <ehdud05264@naver.com>
2023-01-20 21:18:16 +0900

    book_mst

--- library/book_mst.sql
+++ library/book_mst.sql
@@ -1,39 +1,15 @@
 SELECT * FROM library.book_mst;
-
-
+select
+	*
+from
+	category_view
+where
+	category_id <= 5;
 
 SELECT * FROM book_mst order by publication_date desc;
 
-update book_mst
-set publication_date = date_format('2010-11-23', '%Y-%m-%d')
-where publication_date like '1900-01-00';
-
-SELECT
-	*
+delete
 from
-	book_mst 
+	book_mst
 where
-	1 = 1
-    /*
-and	(
-		book_name like '%사용%'
-	or author like '%사용%'
-    or publisher like '%사용%'
-    )
-    */
-and category = '소설'
-order by
-	#book_mst
-    #author
-    #publication_date,
-    book_id
-limit 0, 20;
-
-create view test as
-select
-	username,
-    name
-from
-	user_mst;
-
-
+	book_id in(1,2,3,4,5);
